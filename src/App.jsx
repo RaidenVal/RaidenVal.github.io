3bb11caf78414bcb6f8986798f66260b9fdfc0ab
RaidenVal <jolenezou711@gmail.com>
2026-02-26 10:07:45 +0000

    init: setup React + Tailwind + React Router

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <main className="px-8 py-12">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  )
+}
+
+export default App
